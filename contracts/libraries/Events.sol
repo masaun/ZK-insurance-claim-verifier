a54e9410642832644504e07875c3352402d9d54c
masaun <masaun@protonmail.com>
2025-04-20 20:57:34 +0900

    [SC]: Define the Events library SC (Events.sol)

---
+++ contracts/libraries/Events.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+
+/**
+ * @title Events library
+ */
+library Events {
+    event ClaimApproved(address indexed claimant, uint256 payout);
+    event ClaimDenied(address indexed claimant, string reason);
+    event ClaimSubmitted(address indexed claimant, bytes proof, bytes32[] publicInputs);
+    event HospitalBillDataStored(address indexed hospital, bytes32[] publicInputs);
+    event PayoutProcessed(address indexed claimant, uint256 payoutAmount);
+    event InsuranceClaimProofVerifierUpdated(address indexed newVerifier);
+    event InsurerUpdated(address indexed newInsurer);
+}
