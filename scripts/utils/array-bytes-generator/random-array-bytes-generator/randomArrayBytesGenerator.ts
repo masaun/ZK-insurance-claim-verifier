e9e78be20dee86b2f2be98d1d7e8bf07312b5147
masaun <masaun@protonmail.com>
2025-04-22 10:32:01 +0900

    [SC script]: Rename with the 'randomUint8ArrayBytes' in the randomArrayBytesGenerator.ts

--- scripts/utils/array-bytes-generator/random-array-bytes-generator/randomArrayBytesGenerator.ts
+++ scripts/utils/array-bytes-generator/random-array-bytes-generator/randomArrayBytesGenerator.ts
@@ -2,18 +2,19 @@
  * @notice - Generate a random array of bytes
  */
 async function generateRandomArrayBytes() {
-    const randomBytes = new Uint8Array(10);
-    const randomArrayBytes = Array(64).fill(0);
-    return { randomBytes, randomArrayBytes };
+    const randomUint8ArrayBytes = new Uint8Array(10); // Lenght of 10 bytes
+    const randomArrayBytes = Array(64).fill(0);       // Length of 64 bytes
+
+    return { randomUint8ArrayBytes, randomArrayBytes };
 }
 
 /**
  * @notice - The main function
  */
 async function main() { // Mark the function as async
-    const { randomBytes, randomArrayBytes } = await generateRandomArrayBytes();
-    console.log(`randomBytes (via Uint8Array(10)): ${ randomBytes }`);
-    console.log(`randomArrayBytes (via Array(64).fill(0)): ${ randomArrayBytes }`);
+    const { randomUint8ArrayBytes, randomArrayBytes } = await generateRandomArrayBytes();
+    console.log(`randomUint8ArrayBytes (via Uint8Array(10) + Lenght of 10 bytes): ${ randomUint8ArrayBytes }`);
+    console.log(`randomArrayBytes (via Array(64).fill(0) + Length of 64 bytes): ${ randomArrayBytes }`);
 }
 
 /**
