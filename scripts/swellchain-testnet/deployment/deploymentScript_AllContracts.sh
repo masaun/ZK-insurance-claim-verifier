1b223fb09851c7ec450b8ed1dfec4ce7e374e975
masaun <masaun@protonmail.com>
2025-04-24 11:36:42 +0900

    [SC script - deployment]: Add the DeploymentAllContracts.s.sol to the ./scripts/swellchain-testnet/deployment

---
+++ scripts/swellchain-testnet/deployment/deploymentScript_AllContracts.sh
@@ -0,0 +1,22 @@
+echo "Load the environment variables from the .env file..."
+. ./.env
+
+echo "Deploying the UltraVerifier contract on Swell Chain Testnet..."
+forge script script/swell-chain-testnet/deployment/DeploymentAllContracts.s.sol \
+    --broadcast \
+    --rpc-url ${SWELL_CHAIN_TESTNET_RPC} \
+    --chain-id ${SWELL_CHAIN_CHAIN_ID} \
+    --private-key ${SWELL_CHAIN_TESTNET_PRIVATE_KEY} \
+    ./contracts/circuit/ultra-verifier/plonk_vk.sol:UltraVerifier \
+    ./contracts/circuit/InsuranceClaimProofVerifier.sol:InsuranceClaimProofVerifier \
+    --skip-simulation
+
+echo "Verify the deployed-smart contracts (icl. UltraVerifier, InsuranceClaimProofVerifier) on Swell Chain Testnet Explorer..."
+forge script script/edu-chain-testnet/deployment/DeploymentAllContracts.s.sol \
+    --rpc-url ${SWELL_CHAIN_TESTNET_RPC} \
+    --chain-id ${SWELL_CHAIN_CHAIN_ID} \
+    --private-key ${SWELL_CHAIN_TESTNET_PRIVATE_KEY} \
+    --resume \
+    --verify \
+    --verifier blockscout \
+    --verifier-url https://swell-testnet-explorer.alt.technology/api/
