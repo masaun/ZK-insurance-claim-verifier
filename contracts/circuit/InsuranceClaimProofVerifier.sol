b843d641a47c018302d8c5db68f19b9fee896453
masaun <masaun@protonmail.com>
2025-07-22 22:12:28 +0900

    [SC]: Define the 'version' and pock()

--- contracts/circuit/InsuranceClaimProofVerifier.sol
+++ contracts/circuit/InsuranceClaimProofVerifier.sol
@@ -4,15 +4,23 @@ import { UltraVerifier } from "./ultra-verifier/plonk_vk.sol";
 //import "../circuits/target/contract.sol";
 
 contract InsuranceClaimProofVerifier {
+
+    string public version;
+
     UltraVerifier public verifier;
 
     constructor(UltraVerifier _verifier) {
         verifier = _verifier;
+        version = "0.1.0";
     }
 
     function verifyInsuranceClaimProof(bytes calldata proof, bytes32[] calldata publicInputs) public view returns (bool) {
         bool proofResult = verifier.verify(proof, publicInputs);
         require(proofResult, "A given InsuranceClaimProof is not valid");
         return proofResult;
+    }
+
+    function pock() public returns (bool) {
+        return true;
     }
 }
