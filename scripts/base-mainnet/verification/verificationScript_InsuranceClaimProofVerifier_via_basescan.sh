5ba320c68e5188421d0f98ab7e24a33a5c9acd74
masaun <masaun@protonmail.com>
2025-07-26 10:54:33 +0900

    [SC script]: Add the SC verification script via BlockScout

--- scripts/base-mainnet/verification/verificationScript_InsuranceClaimProofVerifier_via_basescan.sh
+++ scripts/base-mainnet/verification/verificationScript_InsuranceClaimProofVerifier_via_basescan.sh
@@ -4,11 +4,11 @@ source .env
 #. ./.env
 
 # @notice - [Result]: Successfully "verified" the InsuranceClaimProofVerifier contract on Base Mainnet. 
-echo "Verifying the InsuranceClaimProofVerifier contract on Base Mainnet..."
+echo "Verifying the InsuranceClaimProofVerifier contract on Base Mainnet (via BaseScan)..."
 forge verify-contract \
     --rpc-url ${BASE_MAINNET_RPC} \
     --chain-id ${BASE_MAINNET_CHAIN_ID} \
-    0x9d04136bEaA01d30Db51BD8F79232633A86E4B80 \
+    ${INSURANCE_CLAIM_PROOF_VERIFIER_ON_BASE_MAINNET} \
     ./contracts/circuit/InsuranceClaimProofVerifier.sol:InsuranceClaimProofVerifier \
     --etherscan-api-key ${BASESCAN_API_KEY}
 
