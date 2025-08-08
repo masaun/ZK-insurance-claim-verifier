0428ca7df0454ff35fb979dc683dd129362f0f8b
masaun <masaun@protonmail.com>
2025-08-08 13:33:55 +0900

    [SC]: Update each SC versions

--- contracts/InsuranceClaimManager.sol
+++ contracts/InsuranceClaimManager.sol
@@ -21,7 +21,7 @@ contract InsuranceClaimManager {
 
     constructor(InsuranceClaimProofVerifier _insuranceClaimProofVerifier) {
         insuranceClaimProofVerifier = _insuranceClaimProofVerifier;
-        version = "0.1.4";
+        version = "0.1.5";
     }
 
     /**
