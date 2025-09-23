8520faa468f37f34c39b06dd59dcdb8ba7a3764a
masaun <masaun@protonmail.com>
2025-09-23 13:24:11 +0900

    [SC script]: Copy the /calling-functions directory from the /base-mainnet to the /celo-mainnet directory

---
+++ scripts/celo-mainnet/calling-functions/rs/base-mainnet/batch-call/runningScript_InsuranceClaimManager.sh
@@ -0,0 +1,5 @@
+echo "Load the environment variables from the .env file..."
+source .env
+
+echo "Running the InsuranceClaimManager contract interactions..."
+cargo run --bin insurance_claim_manager_on_base_mainnet -- --show-output any_network
